5c43a45edfd815d7332cba424f574551b37cdae2
HalikD <khalikdamir@yandex.ru>
2023-05-25 23:38:22 +0500

    add base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import fs from 'fs/promises'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/renue-test-front',
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.js/,
@@ -29,7 +30,6 @@ export default defineConfig({
   // },
   build: {
     minify: true,
-    outDir: 'build',
   },
   plugins: [react()],
   resolve: {
